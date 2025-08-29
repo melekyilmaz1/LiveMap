848e55c11940026040b095d8222743aeddff8b61
melekyilmaz1 <meleky2307@gmail.com>
2025-08-29 12:29:04 +0300

    Initial Commit

---
+++ Assigment2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assigment2
+//
+//  Created by Melek Yılmaz on 29.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
